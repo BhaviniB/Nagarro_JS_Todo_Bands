4b86b5632b4c1136ab8e5e8e48a7b017da422405
Bhavini Batra <lassiebhavini@gmail.com>
2019-10-11 23:04:23 +0530

    Configuring db

---
+++ config/database.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb://BhaviniBatraBcube:Bcube@cluster0-shard-00-00-l4gsp.mongodb.net:27017,cluster0-shard-00-01-l4gsp.mongodb.net:27017,cluster0-shard-00-02-l4gsp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
